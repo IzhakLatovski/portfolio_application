2467cd8f73674dee1515623168f17bdbbbc29ed7
user name <user email>
2021-12-21 19:30:07 +0200

    Checking Jenkins pipeline

--- appsmall.py
+++ appsmall.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def index():
     if request.method == 'GET':
-        return "HELLO FROM MY WEBSITE"
+        return "HELLO FROM MY WEBSITE 222"
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
